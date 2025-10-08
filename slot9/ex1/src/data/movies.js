5085d1dafe963611ace0d345b845d0d0a13ff51d
Phuong-Nguyennn <ducphuong99821@gmail.com>
2025-10-08 11:22:18 +0700

    Push toàn bộ folder Code

---
+++ slot9/ex1/src/data/movies.js
@@ -0,0 +1,38 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Galactic Wars",
+    description:
+      "Epic space battles decide the fate of a fractured galaxy as rival factions clash for control of star systems and ancient technologies.",
+    poster: "/images/movie1.jpg",
+    genre: "Sci-Fi",
+    year: 2022,
+    country: "USA",
+    duration: 132,
+    showtimes: ["10:00", "14:00", "19:30"]
+  },
+  {
+    id: 2,
+    title: "Laugh Out Loud",
+    description:
+      "A feel-good comedy about friendship, failure, and second chances in a small town full of surprises.",
+    poster: "/images/movie2.jpg",
+    genre: "Comedy",
+    year: 2021,
+    country: "UK",
+    duration: 105,
+    showtimes: ["09:00", "13:30", "20:15"]
+  },
+  {
+    id: 3,
+    title: "Deep Blue",
+    description:
+      "A gripping survival drama set far from shore when a voyage goes wrong and reveals human resilience.",
+    poster: "/images/movie3.jpg",
+    genre: "Drama",
+    year: 2019,
+    country: "Australia",
+    duration: 118,
+    showtimes: ["11:15", "17:45", "21:00"]
+  }
+];
